4f02e46bb549bb3ee605075badcda8c9c3cbfb7b
Steve <stevenlyall@gmail.com>
2016-01-27 22:01:22 -0800

    test structure

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-/**
- * Created by stevenlyall on 2016-01-27.
- */
+var express = require('express');
+var app = express();
+
+var logger = require('morgan');
+app.use(logger('dev'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+app.use('/api', router);
+
+router.get('/', function(req, res) {
+	res.json({ message: 'hello world' });
+});
+
+
+
+app.listen(port);
