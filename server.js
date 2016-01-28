cca576c2e57d7551152bfed58f2fffa65984e2e6
Steve <stevenlyall@gmail.com>
2016-01-27 22:22:50 -0800

    update

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var app = express();
 var logger = require('morgan');
 app.use(logger('dev'));
 
+var db = require('rethinkdb');
+var connection = null;
+db.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+	if (err) throw err;
+	connection = conn;
+});
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,7 +21,7 @@ var router = express.Router();
 app.use('/api', router);
 
 router.get('/', function(req, res) {
-	res.json({ message: 'hello world' });
+	res.json({ message: 'hello world'});
 });
 
 
